9d3b3e939f0d0a3e582c305e48664e6643e195e3
fabianalmanza <fabian.almanza741@gmail.com>
2023-07-29 03:52:11 -0500

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+
+export function Footer () {
+  // const { filters } = useFilters()
+
+  return (
+    <footer className='footer'>
+      <h4>React ⚛️ － <span>Fabian Almanza</span></h4>
+      <h5>Shopping Cart con useContext & useReducer</h5>
+    </footer>
+  )
+}
